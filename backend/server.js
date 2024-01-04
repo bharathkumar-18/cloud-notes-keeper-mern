fd922c91cd52f4f98e0522ebd8705a66ed18e5f9
Bharath Kumar <bharathoppobtc@gmail.com>
2024-01-04 22:16:36 +0530

    Tried dynamic route

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const notes = require('./data/notes');
 
 const app = express();
 
@@ -6,4 +7,9 @@ app.listen(5000,console.log("Running on PORT 5000"));
 
 app.get('/',(req,res) => {
     res.send("API is running");
+})
+
+app.get('/api/notes/:id',(req,res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note);
 })
