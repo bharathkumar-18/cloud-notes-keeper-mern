e0f506fa7b2b5565b6e5ba0c000588cbeea28f1e
Bharath Kumar <bharathoppobtc@gmail.com>
2024-01-08 20:19:23 +0530

    Connected frontend with backend and removed CORS error

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.get('/',(req,res) => {
 
 const PORT = process.env.PORT || 6000;
 
+app.get('/api/notes', (req,res)=>{
+    res.send(notes);
+})
+
 app.get('/api/notes/:id',(req,res) => {
     const note = notes.find((n) => n._id === req.params.id);
     res.send(note);
--- frontend/src/Screens/MyNotes/MyNotes.js
+++ frontend/src/Screens/MyNotes/MyNotes.js
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainScreen from '../../components/MainScreen'
 import './MyNotes.css';
-import notesData from '../../data/notesData.js';
+import axios from 'axios';
 
 
 const MyNotes = () => {
 
-  function toggleDescription(event){
-    const heading = event.target;
-    const description = event.target.nextElementSibling;
+  const [notes, setNotes] = useState([]);
+
+  const fetchNotes = async ()=>{
+    const reqData = await axios.get('/api/notes');
+    const notes = reqData.data;
+    console.log(notes);
+    setNotes(notes);
+  }
+
+  useEffect(()=>{
+    fetchNotes();
+  }, []);
+
+  function toggleDescription(e){
+    const heading = e.target.parentNode;
+    const description = heading.nextElementSibling;
+    const bookLogo = heading.querySelector('.book-logo');
     console.log(heading);
     console.log(description);
   if (description.classList.contains("show-note-description")) {
     heading.classList.remove("add-border");
     description.classList.remove("show-note-description");
+    bookLogo.innerHTML = "📘";
   } else {
     heading.classList.add("add-border");
     description.classList.add("show-note-description");
+    bookLogo.innerHTML = "📖";
   }
+  }
+
+  //Function to delete a particular note with it's ID
+  const deleteHandler = ()=>{
+   console.log("OK");
   }
 
   return (
@@ -27,19 +48,24 @@ const MyNotes = () => {
       </button>
 
       {
-        notesData.map((note)=>(
+        notes.map((note)=>(
             // Each note is being rendered with data from backend and Frontend
               //Card Component
-              <div key={note._id} className='note w-full border-black border-2 p-0 m-4' onClick={toggleDescription}>
-                <div className='note-head h-12 flex items-center justify-between bg-neutral-300 px-8'>
-                  <h1 className='note-heading'>{note.title}</h1>
-                    <div className='modify-btns'>
-                      <button className='h-9 w-20 bg-black text-white text-xl rounded-md mx-16'>Edit</button>
-                      <button className='h-9 w-20 bg-red-500 text-white text-xl rounded-md'>Delete</button>
+              <div key={note._id} className='note w-11/12 border-black border-2 p-0 mx-auto my-4' >
+                <div className='note-head h-12 flex items-center justify-between bg-neutral-300 px-8' >
+                  <h1 className='note-heading w-full ' onClick={toggleDescription}><span className='book-logo'>📘</span>  {note.title}</h1>
+                    <div className='modify-btns border-black border-l-2 flex'>
+                      <button className='h-9 w-20 bg-black text-white text-xl rounded-md mx-16'>
+                        <a href={`/note/${note._id}`}>Edit</a>
+                      </button>
+                      <button className='h-9 w-20 bg-red-500 text-white text-xl rounded-md' onClick={deleteHandler}>Delete</button>
                     </div>
                 </div>
                 <div className='note-description' id='note-description'>
+                    <h1 className=' w-fit bg-green-500 text-white p-1 rounded-md'>Category - {note.category}</h1><br/>
                     <p>{note.content}</p>
+                    <br/><hr/><br/>
+                    <p className=' text-gray-400'>-- Created on -Date</p>
                 </div>
             </div>
             // End of Card component
